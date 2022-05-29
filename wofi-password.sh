db4a3bed1b463ce47b25be18954e4e5d9086c92d
DNDEBUG <98880063+DNDEBUG@users.noreply.github.com>
2022-05-29 20:08:07 +0000

    Update wofi-password.sh

--- wofi-password.sh
+++ wofi-password.sh
@@ -3,4 +3,4 @@ PASSDIR="INSERT_DIR_HERE"
 PASSFILE=$(ls $PASSDIR | wofi --show dmenu)
 ITER=$(wofi --dmenu -p iter --normal-window -L 1 --password)
 PASS=$(wofi --dmenu -p password --normal-window -L 1 --password)
-openssl aes256 -d -iter $ITER -in $PASSFILE -k $PASS | wofi --show dmenu | awk '{printf $2}' | wl-copy
+openssl aes256 -d -iter $ITER -in $PASSDIR/$PASSFILE -k $PASS | wofi --show dmenu | awk '{printf $2}' | wl-copy
