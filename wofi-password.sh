c021a17e8d954d447c3f8a666d99491bbc268654
DNDEBUG <98880063+DNDEBUG@users.noreply.github.com>
2022-05-30 21:41:41 +0000

    Update wofi-password.sh

--- wofi-password.sh
+++ wofi-password.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PASSDIR="INSERT_DIR_HERE"
 PASSFILE=$(ls $PASSDIR | wofi --show dmenu)
-PASS=$(wofi --dmenu -p password --normal-window -L 1 --password)
+PASS=$(wofi --dmenu -p Password --normal-window -L 1 --password)
 openssl aes256 -d -in $PASSDIR/$PASSFILE -k $PASS -pbkdf2 | wofi --show dmenu | awk '{printf $2}' | wl-copy
